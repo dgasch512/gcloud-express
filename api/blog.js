255ca878069cc754b28d3ca6cae60b74f5d35dc3
dgasch512 <gaschdav208@gmail.com>
2020-08-05 21:29:40 -0500

    blog route bug fix

--- api/blog.js
+++ api/blog.js
@@ -2,18 +2,10 @@
 const handleBlogGet = (req, res, db) => {
   db('stories')
     .returning('*')
-    .then(data => {
-      if (data.length) {
-       res.json(data) 
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => (req, res) => {
-      console.log('This is it')
+    .then(story => {
+      res.json(story)
     })
 }
-
 
 
 module.exports = {
