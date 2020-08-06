0320ebf780b309da533db926af6e2a10508c7159
dgasch512 <gaschdav208@gmail.com>
2020-08-05 20:28:41 -0500

    db test

--- index.js
+++ index.js
@@ -15,24 +15,24 @@ const admin = require('./api/admin');
 const adminId = require('./api/adminId');
 const rideTotal = require('./api/rideTotal');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true
-  }
-});
-
 // const db = knex({
 //   client: 'pg',
 //   connection: {
-//     host : 'ec2-23-22-156-110.compute-1.amazonaws.com',
-//     user : 'cgnxwzxluttpkj',
-//     password : 'cd1500ab612a546f9af5e019158eb4104045da5827155566705de354632f7f16',
-//     database : 'dk9agg1b3hol7',
+//     connectionString : process.env.DATABASE_URL,
 //     ssl: true
 //   }
 // });
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : 'ec2-18-211-48-247.compute-1.amazonaws.com',
+    user : 'hstgbiqbfaxwxt',
+    password : '51b1445f8385ff20353e8900443659a331529818e032f4e1fa97c468d83201e1',
+    database : 'dbg1qhf7gfbev5',
+    ssl: true
+  }
+});
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const port = process.env.PORT || 4000;
@@ -43,13 +43,13 @@ app.set('db', db);
 app.use(bodyParser.json());
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-} 
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+// } 
 
 
 app.options('/', cors())
-app.get('/', (req,res) => { res.send('it is working')})
+app.get('/', (req,res) => { res.send(db.drivers)})
 app.post('/login', (req,res) => { login.handleLogin(req, res, db, bcrypt)});
 app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt)});
 app.options('/blog', cors());
@@ -57,7 +57,7 @@ app.get('/blog', (req,res) => { blog.handleBlogGet(req, res, db)});
 app.post('/request', (req, res) => { request.handleRequest(req, res, db)});
 app.post('/artpost', (req, res) => { postart.handlePostArt(req, res, db)});
 app.options('/admin', cors())
-app.get('/admin', (req, res) => { admin.handleRidesGet(req, res, db)});``
+app.get('/admin', (req, res) => { admin.handleRidesGet(req, res, db)});
 app.get('/admin/:id', (req, res) => { adminId.handleAdminID(req, res, db)});
 app.put('/ride-total', (req, res) => { rideTotal.rideTotal(req, res, db)});
 
